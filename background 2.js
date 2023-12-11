c402f4d28db94ca7f24fa84b568168be8e784972
AnukruthiAtluri <148026179+AnukruthiAtluri@users.noreply.github.com>
2023-12-10 23:04:39 -0500

    RC Checkpoint Commit

--- background 2.js
+++
@@ -1,5 +0,0 @@
-chrome.runtime.onInstalled.addListener((details) => {
-  if (details.reason === "install") {
-    chrome.tabs.create({ url: "https://dev.dk5o9ng78c4g4.amplifyapp.com/" });
-  }
-});
--- content 2.js
+++
@@ -1,2 +0,0 @@
-console.log("FormAI content script loaded");
-// Your form manipulation logic will go here
